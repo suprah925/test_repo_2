1b0fbd6aa339bea75e5d372704ee6ace03f67fd3
suprah925 <141354117+suprah925@users.noreply.github.com>
2023-09-05 16:22:36 -0700

    Create test_2.js

---
+++ test_2.js
@@ -0,0 +1,27 @@
+// Error 1: Missing parentheses
+console.log "Hello, World!");
+
+// Error 2: Misspelled variable name
+let mesage = "This is a message";
+console.log(message);
+
+// Error 3: Undefined variable
+console.log(nonExistentVariable);
+
+// Error 4: Using reserved keyword as variable
+let true = false;
+console.log(true);
+
+// Error 5: Unclosed string
+let text = 'This is an unclosed string;
+
+// Error 6: Incorrect operator
+let result = 10 ÷ 2;
+console.log(result);
+
+// Error 7: Trying to access a property of undefined
+let obj = null;
+console.log(obj.property);
+
+// Error 8: Using an undeclared function
+hello();
